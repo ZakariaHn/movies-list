cebc1c727469406d7793d9ac5e2f6d90b38eac64
zak258 <zakaria.k.hamdan@gmail.com>
2020-05-01 15:00:06 +0200

    Updates

---
+++ scripts/arrayOfObjects.js
@@ -0,0 +1,72 @@
+let moviesData = [
+  {
+    id: "theMaster",
+    title: "The Master",
+    year: "2012",
+
+    description:
+      "Paul Thomas Anderson's 'The Master' is fabulously well-acted and crafted, but when I reach for it, my hand closes on air. It has rich material and isn't clear what it thinks about it. It has two performances of Oscar caliber, but do they connect? Its title character is transparently inspired by L. Ron Hubbard, the founder of Scientology, but it sidesteps any firm vision of the cult religion itself — or what it grew into The Hubbard character, named Lancaster Dodd and played by Philip Seymour Hoffman, is indeed not even the film 's most important. Top billing goes to an alcoholic adherent of The Master named Freddie Quell — played by Joaquin Phoenix, who in some ways seems to flow out of the bizarre persona he created during his meltdown, or whatever it was, two years ago. ",
+    director: "Paul Thomas Anderson",
+    image: "./img/themaster.jpg",
+  },
+  {
+    id: "baraka",
+    title: "Baraka",
+    year: "1992",
+
+    description:
+      "If man sends another Voyager to the distant stars and it can carry only one film on board, that film might be 'Baraka.' It uses no language, so needs no translation. It speaks in magnificent images, natural sounds, and music both composed and discovered. It regards our planet and the life upon it. It stands outside of historical time. To another race, it would communicate: This is what you would see if you came here. Of course this will all long since have disappeared when the spacecraft is discovered.",
+    director: "Ron Fricke",
+    image: "./img/Baraka.jpg",
+  },
+  {
+    id: "eternal",
+    title: "Eternal Sunshine of the Spotless Mind",
+    year: "1976",
+
+    description:
+      "Visiting an old people's home, I walked down a corridor on the floor given over to advanced Alzheimer's parents. Some seemed anxious. Some were angry. Some simply sat there. Knowing nothing of what was happening in their minds, I wondered if the anxious and angry ones had some notion of who they were and that something was wrong. I was reminded of the passive ones while watching 'Eternal Sunshine of the Spotless Mind.' Wiped free of memory, they exist always in the moment, which they accept because it is everything.",
+    director: "Michel Gondry",
+    image: "./img/eternalSunshin.jpg",
+  },
+  {
+    id: "baath",
+    title: "A Flood in the Baath Country",
+    year: "2005",
+
+    description:
+      "The documentary explores the implications of Baath Party policies in Syria. Without commentary or criticism, Amiralay's film exposes Baath party propaganda and its debilitating effects on the people of al-Mashi village, 400 kilometers (250 mi) northeast of Damascus. The film moves slowly from students to teachers to government officials, with everyone reciting exactly the same praise for the president and slogans glorifying the Baath party. The film is a harsh indictment of the regime, portraying the devastating effects of 35 years of rigid Baath party rule on Syrian society.",
+    director: "Omar Amiralay",
+    image: "./img/baath.jpg",
+  },
+  {
+    id: "secondeFloor",
+    title: "Songs from the Second Floor",
+    year: "2000",
+
+    description:
+      "You have never seen a film like this before. You may not enjoy it but you will not forget it. Andersson is a deadpan Swedish surrealist who has spent the last 25 years making 'the best TV commercials in the world' Ingmar Bergman and now bites off the hand that fed him, chews it thoughtfully, spits it out and tramples on it. His movie regards modern capitalist society with the detached hilarity of a fanatic saint squatting on his pillar in the desert.",
+    director: "Roy Andersson",
+    image: "./img/songsFromTheSecondFloor.jpg",
+  },
+  {
+    id: "persepolis",
+    title: "Persepolis",
+    year: "2007",
+
+    description:
+      "You have never seen a film like this before. You may not enjoy it but you will not forget it. Andersson is a deadpan Swedish surrealist who has spent the last 25 years making 'the best TV commercials in the world' Ingmar Bergman and now bites off the hand that fed him, chews it thoughtfully, spits it out and tramples on it. His movie regards modern capitalist society with the detached hilarity of a fanatic saint squatting on his pillar in the desert.",
+    director: "Vincent Paronnaud $ Marjane Satrapi",
+    image: "./img/persepolis.jpg",
+  },
+  {
+    id: "theLobster",
+    title: "The Lobster",
+    year: "2015",
+
+    description:
+      "You have never seen a film like this before. You may not enjoy it but you will not forget it. Andersson is a deadpan Swedish surrealist who has spent the last 25 years making 'the best TV commercials in the world' Ingmar Bergman and now bites off the hand that fed him, chews it thoughtfully, spits it out and tramples on it. His movie regards modern capitalist society with the detached hilarity of a fanatic saint squatting on his pillar in the desert.",
+    director: "Yorgos Lanthimos",
+    image: "./img/thelobster.jpg",
+  },
+];
--- scripts/index.js
+++ scripts/index.js
@@ -1,69 +1,23 @@
-let theMaster = document.getElementById("theMaster");
-(baraka = document.getElementById("baraka")),
-  (theMaster = document.getElementById("theMaster")),
-  (baath = document.getElementById("baath")),
-  (secondeFloor = document.getElementById("secondeFloor")),
-  (persepolis = document.getElementById("persepolis")),
-  (thesLobster = document.getElementById("thesLobster"));
+const titleHtml = document.getElementById("title"),
+  directorHtml = document.getElementById("director"),
+  yearHtml = document.getElementById("year"),
+  imgHtml = document.querySelector("img"),
+  cardBody = document.querySelector(".card__body"),
+  listItems = document.querySelector(".list").querySelectorAll("div");
 
-//
-let list = document.querySelector(".list *");
-console.log(list);
-let cardFront = document.getElementById("cardFront");
-let cardBody = document.querySelector(".card__body");
+for (let i = 0; i < listItems.length; i++) {
+  listItems[i].addEventListener("click", changes);
+}
 
-//
-theMaster.onclick = function () {
-  cardFront.classList.add("theMasterImg");
-  cardBody.innerHTML =
-    "Paul Thomas Anderson's 'The Master' is fabulously well-acted and crafted, but when I reach for it, my hand closes on air. It has rich material and isn't clear what it thinks about it. It has two performances of Oscar caliber, but do they connect? Its title character is transparently inspired by L. Ron Hubbard, the founder of Scientology, but it sidesteps any firm vision of the cult religion itself — or what it grew into The Hubbard character, named Lancaster Dodd and played by Philip Seymour Hoffman, is indeed not even the film 's most important. Top billing goes to an alcoholic adherent of The Master named Freddie Quell — played by Joaquin Phoenix, who in some ways seems to flow out of the bizarre persona he created during his meltdown, or whatever it was, two years ago. ";
-  this.style.backgroundColor = "#082e4d";
-};
-
-//
-baraka.onclick = function () {
-  cardFront.classList.add("barakaImg");
-  cardBody.innerHTML =
-    "If man sends another Voyager to the distant stars and it can carry only one film on board, that film might be 'Baraka.' It uses no language, so needs no translation. It speaks in magnificent images, natural sounds, and music both composed and discovered. It regards our planet and the life upon it. It stands outside of historical time. To another race, it would communicate: This is what you would see if you came here. Of course this will all long since have disappeared when the spacecraft is discovered.";
-  this.style.backgroundColor = "#082e4d";
-};
-
-//
-eternal.onclick = function () {
-  cardFront.classList.add("eternalImg");
-  cardBody.innerHTML =
-    "Visiting an old people's home, I walked down a corridor on the floor given over to advanced Alzheimer's parents. Some seemed anxious. Some were angry. Some simply sat there. Knowing nothing of what was happening in their minds, I wondered if the anxious and angry ones had some notion of who they were and that something was wrong. I was reminded of the passive ones while watching 'Eternal Sunshine of the Spotless Mind.' Wiped free of memory, they exist always in the moment, which they accept because it is everything.";
-  this.style.backgroundColor = "#082e4d";
-};
-
-//
-baath.onclick = function () {
-  cardFront.classList.add("baathImg");
-  cardBody.innerHTML =
-    "The documentary explores the implications of Baath Party policies in Syria. Without commentary or criticism, Amiralay's film exposes Baath party propaganda and its debilitating effects on the people of al-Mashi village, 400 kilometers (250 mi) northeast of Damascus. The film moves slowly from students to teachers to government officials, with everyone reciting exactly the same praise for the president and slogans glorifying the Baath party. The film is a harsh indictment of the regime, portraying the devastating effects of 35 years of rigid Baath party rule on Syrian society.";
-  this.style.backgroundColor = "#082e4d";
-};
-
-//
-secondeFloor.onclick = function () {
-  cardFront.classList.add("secondeFloorImg");
-  cardBody.innerHTML =
-    "You have never seen a film like this before. You may not enjoy it but you will not forget it. Andersson is a deadpan Swedish surrealist who has spent the last 25 years making 'the best TV commercials in the world' (Ingmar Bergman) and now bites off the hand that fed him, chews it thoughtfully, spits it out and tramples on it. His movie regards modern capitalist society with the detached hilarity of a fanatic saint squatting on his pillar in the desert.";
-  this.style.backgroundColor = "#082e4d";
-};
-
-//
-persepolis.onclick = function () {
-  cardFront.classList.add("persepolisImg");
-  cardBody.innerHTML =
-    "I attended the Tehran Film Festival in 1972 and was invited to the home of my guide and translator to meet her parents and family. Over tea and elegant pastries, they explained proudly that Iran was a 'modern' country, that they were devout Muslims but did not embrace the extremes of other Islamic nations, that their nation represented a new way. Whenever I read another story about the clerical rule that now grips Iran, I think of those people, and millions of other Iranians like them, who do not agree with the rigid restrictions they live under, particularly the women. Iranians are no more monolithic than we are, a truth not grasped by our own zealous leader. Remember, on 9/11 there was a huge candlelight vigil in Tehran in sympathy with us.";
-  this.style.backgroundColor = "#082e4d";
-};
-
-//
-thesLobster.onclick = function () {
-  cardFront.classList.add("thesLobsterImg");
-  cardBody.innerHTML =
-    "'The Lobster,' a black-hearted flat-affect comedy from Greek director Yorgos Lanthimos, making his English-language debut, presents a dystopian world where being single is a criminal act. A romantic breakup thrusts the 'single' into the outer darkness of society. (Like all good satire, 'The Lobster' is close enough to reality to disturb the waters.) A single person has 45 days after a breakup to find a new partner, and if the new partner does not materialize, the single person will be turned into an animal. The message is clear: Couples deserve official protection, and the privilege of being left alone by the unnamed State. Single people are accosted in public spaces with demands for appropriate papers. 'The Lobster' plays rigorously by its own rules without once telegraphing 'Just kidding!' While extremely funny, it is a bitter and ruthless film. Lanthimos plays target practice and his aim is deadly.";
-  this.style.backgroundColor = "#082e4d";
-};
+function changes() {
+  for (let i = 0; i < listItems.length; i++) {
+    this.style.backgroundColor = "#082e4d";
+    listItems[i].removeAttribute("style");
+  }
+  let data = moviesData.find((data) => data.id == this.id);
+  titleHtml.innerHTML = `<h4>${data.title}</h4>`;
+  yearHtml.innerHTML = data.year;
+  directorHtml.innerHTML = data.director;
+  cardBody.innerHTML = data.description;
+  imgHtml.src = data.image;
+}
