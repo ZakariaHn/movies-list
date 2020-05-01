dcb57f29288c40e92a1350456831774a9736ae92
zak258 <zakaria.k.hamdan@gmail.com>
2020-05-01 15:37:29 +0200

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -12,12 +12,10 @@ listItems.forEach((item) => {
 function changes() {
   for (let item of listItems) {
     this.style.backgroundColor = "#082e4d";
+    this.style.transform = "translateX(2rem)";
     item.removeAttribute("style");
   }
-
   let data = moviesData.find((data) => data.id == this.id);
-  titleHtml.innerHTML = `<h4>${data.title}</h4>`;
-  yearHtml.innerHTML = data.year;
   directorHtml.innerHTML = data.director;
   cardBody.innerHTML = data.description;
   imgHtml.src = data.image;
