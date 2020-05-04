d212468488dd05223f757d65cbf407b6805a27c9
zak258 <zakaria.k.hamdan@gmail.com>
2020-05-04 11:25:28 +0200

    Updates

--- scripts/data.js
+++ scripts/data.js
@@ -1,48 +1,58 @@
+let data = JSON.stringify(moviesData);
+export { data };
+
 let moviesData = [
   {
-    name: "theMaster",
+    id: "001",
+    title: "The Master",
     director: "Directed by Paul Thomas Anderson, 2012.",
     image: "./img/themaster.jpg",
     description:
       "Paul Thomas Anderson's 'The Master' is fabulously well-acted and crafted, but when I reach for it, my hand closes on air. It has rich material and isn't clear what it thinks about it. It has two performances of Oscar caliber, but do they connect? Its title character is transparently inspired by L. Ron Hubbard, the founder of Scientology, but it sidesteps any firm vision of the cult religion itself — or what it grew into The Hubbard character, named Lancaster Dodd and played by Philip Seymour Hoffman, is indeed not even the film 's most important. Top billing goes to an alcoholic adherent of The Master named Freddie Quell — played by Joaquin Phoenix, who in some ways seems to flow out of the bizarre persona he created during his meltdown, or whatever it was, two years ago. ",
   },
   {
-    name: "baraka",
+    id: "002",
+    title: "Baraka",
     director: "Directed by Ron Fricke, 1992.",
     image: "./img/Baraka.jpg",
     description:
       "If man sends another Voyager to the distant stars and it can carry only one film on board, that film might be 'Baraka.' It uses no language, so needs no translation. It speaks in magnificent images, natural sounds, and music both composed and discovered. It regards our planet and the life upon it. It stands outside of historical time. To another race, it would communicate: This is what you would see if you came here. Of course this will all long since have disappeared when the spacecraft is discovered.",
   },
   {
-    name: "eternal",
+    id: "003",
+    title: "Eternal Sunshine of the Spotless Mind",
     director: "Directed by Michel Gondry, 2004.",
     image: "./img/eternalSunshin.jpg",
     description:
       "Visiting an old people's home, I walked down a corridor on the floor given over to advanced Alzheimer's parents. Some seemed anxious. Some were angry. Some simply sat there. Knowing nothing of what was happening in their minds, I wondered if the anxious and angry ones had some notion of who they were and that something was wrong. I was reminded of the passive ones while watching 'Eternal Sunshine of the Spotless Mind.' Wiped free of memory, they exist always in the moment, which they accept because it is everything.",
   },
   {
-    name: "baath",
+    id: "004",
+    title: "A Flood in the Baath Country",
     director: "Directed by Omar Amiralay, 2005.",
     image: "./img/baath.jpg",
     description:
       "The documentary explores the implications of Baath Party policies in Syria. Without commentary or criticism, Amiralay's film exposes Baath party propaganda and its debilitating effects on the people of al-Mashi village, 400 kilometers (250 mi) northeast of Damascus. The film moves slowly from students to teachers to government officials, with everyone reciting exactly the same praise for the president and slogans glorifying the Baath party. The film is a harsh indictment of the regime, portraying the devastating effects of 35 years of rigid Baath party rule on Syrian society.",
   },
   {
-    name: "secondeFloor",
+    id: "005",
+    title: "Songs from the Second Floor",
     director: "Directed by Roy Andersson, 2000.",
     image: "./img/songsFromTheSecondFloor.jpg",
     description:
       "You have never seen a film like this before. You may not enjoy it but you will not forget it. Andersson is a deadpan Swedish surrealist who has spent the last 25 years making 'the best TV commercials in the world' Ingmar Bergman and now bites off the hand that fed him, chews it thoughtfully, spits it out and tramples on it. His movie regards modern capitalist society with the detached hilarity of a fanatic saint squatting on his pillar in the desert.",
   },
   {
-    name: "persepolis",
+    id: "006",
+    title: "Persepolis",
     director: "Directed by Vincent Paronnaud & Marjane Satrapi, 2007.",
     image: "./img/persepolis.jpg",
     description:
       "That was the Iran that Marjane Satrapi was born into in 1969, and it was the Iran that ended in the late 1970s with the fall and exile of the shah. Yes, his rule was dictatorial; yes, his secret police were everywhere and his opponents subjected to torture. But that was the norm in the Middle East and in an arc stretching up to the Soviet Union. At least most Iranians were left more or less free to lead the lives they chose. Ironically, many of them believed the fall of the shah would bring more, not less, democracy.",
   },
   {
-    name: "theLobster",
+    id: "007",
+    title: "The Lobster",
     director: "Directed by Yorgos Lanthimos, 2015.",
     image: "./img/thelobster.jpg",
     description:
--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,18 @@
-const titleHtml = document.getElementById("title"),
-  directorHtml = document.getElementById("director"),
-  yearHtml = document.getElementById("year"),
+import { data } from "./data.js";
+let parsedData = JSON.parse(data);
+
+const directorHtml = document.getElementById("director"),
   imgHtml = document.querySelector("img"),
   cardBody = document.querySelector(".card__body"),
   toHover = document.querySelector(".toHover"),
-  listItems = document.querySelector(".list").querySelectorAll("div");
+  listItems = document.querySelector(".list").querySelectorAll("div"),
+  section = document.querySelector(".list");
+
+parsedData.forEach((obj) => {
+  let { id, title } = obj;
+  let card = `<div id = ${id}>${title}</div>`;
+  section.innerHTML += card;
+});
 
 listItems.forEach((item) => {
   item.addEventListener("click", changes);
@@ -17,7 +25,7 @@ function changes() {
     item.removeAttribute("style");
     toHover.style.opacity = "1";
   });
-  let data = moviesData.find((data) => data.name === this.id);
+  let data = moviesData.find((data) => data.id == this.id);
   directorHtml.innerHTML = data.director;
   cardBody.innerHTML = data.description;
   imgHtml.src = data.image;
